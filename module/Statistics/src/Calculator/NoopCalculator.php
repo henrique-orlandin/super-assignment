552fbc9e1c7072a427d31fa81c4a22342351c972
Henrique Orlandin <henrique.orlandin@gmail.com>
2021-11-29 20:49:08 -0800

    set changes back

--- module/Statistics/src/Calculator/NoopCalculator.php
+++ module/Statistics/src/Calculator/NoopCalculator.php
@@ -9,46 +9,19 @@ use Statistics\Dto\StatisticsTo;
 
 class NoopCalculator extends AbstractCalculator
 {
-    protected const UNITS = 'posts';
-
     /**
-     * array with unique users
-     * @var array
-     */
-    private $users = [];
-    /**
-     * total number of posts
-     *
-     * @var integer
-     */
-    private $postCount = 0;
-
-    /**
-     * method that computes information from each post
-     *
-     * @param SocialPostTo $postTo
-     * @return void
+     * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // count the number of posts
-        $this->postCount++;
-
-        // set an array of unique users using the user id
-        $key = $postTo->getAuthorId();
-        $this->users[$key] = $key;
+        // Noops!
     }
 
     /**
-     * calculate average of posts per users from the accumulated data
-     *
-     * @return StatisticsTo
+     * @inheritDoc
      */
     protected function doCalculate(): StatisticsTo
     {
-        // total number of posts divided by the number of users
-        $value = $this->postCount / count($this->users);
-
-        return (new StatisticsTo())->setValue(round($value, 2));
+        return new StatisticsTo();
     }
 }
